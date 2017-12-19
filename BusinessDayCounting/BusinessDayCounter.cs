7a9570788d4bceeae2da37455186351c9b3815ff
Eli <eliesma80@gmail.com>
2017-12-20 08:34:18 +1100

    t1

---
+++ BusinessDayCounting/BusinessDayCounter.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessDayCounting
+{
+    public class BusinessDayCounter
+    {
+        /// <summary>
+        /// TASK ONE:
+        /// Calculates the number of weekdays in between two dates.
+        /// </summary>
+        /// <remarks>
+        /// Weekdays are Monday, Tuesday, Wednesday, Thursday, Friday.
+        /// The returned count should not include either firstDate or secondDate - e.g. between Monday 07-Oct-2013 and Wednesday 09-Oct-2013 is one weekday.
+        /// If secondDate is equal to or before firstDate, return 0.
+        /// </remarks>
+        /// <param name="firstDate">The first date.</param>
+        /// <param name="secondDate">The second date.</param>
+        /// <returns>Number of weekdays</returns>
+        public static int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
+        {
+            int days = 0;
+            firstDate = firstDate.AddDays(1);
+            while (firstDate < secondDate)
+            {
+                if (firstDate.DayOfWeek != DayOfWeek.Saturday && firstDate.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    ++days;
+                }
+                firstDate = firstDate.AddDays(1);
+            }
+            return days;
+            
+        }
+
+        /// <summary>
+        /// TASK TWO:
+        /// Calculates the number of business days in between two dates.
+        /// </summary>
+        /// <remarks>
+        /// Business days are Monday, Tuesday, Wednesday, Thursday, Friday, but excluding any dates which appear in the supplied list of public holidays.
+        /// The returned count should not include either firstDate or secondDate - e.g. between Monday 07-Oct-2013 and Wednesday 09-Oct-2013 is one weekday.
+        /// If secondDate is equal to or before firstDate, return 0.
+        /// </remarks>
+        /// <param name="firstDate">The first date.</param>
+        /// <param name="secondDate">The second date.</param>
+        /// <param name="publicHolidays">List of public holidays.</param>
+        /// <returns>Number of business days</returns>
+        public static int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, IList<DateTime> publicHolidays)
+        {
+            int days = 0;
+            firstDate = firstDate.AddDays(1);
+            while (firstDate < secondDate)
+            {
+                if (firstDate.DayOfWeek != DayOfWeek.Saturday && firstDate.DayOfWeek != DayOfWeek.Sunday)
+                {
+
+                    foreach (DateTime date1 in publicHolidays)
+
+                        if (firstDate == date1) { --days; }
+                    ++days;
+                }
+                firstDate = firstDate.AddDays(1);
+            }
+            return days;
+        }
+
+        public static int BusinessDaysBetweenTwoDatesWithRules(DateTime firstDate, DateTime secondDate, IList<DateTime> publicHolidaysWithRule1, IList<DateTime> publicHolidaysWithRule2, IList<DateTime> publicHolidaysWithRule3)
+        {
+            int days = 0;
+            firstDate = firstDate.AddDays(1);
+            while (firstDate < secondDate)
+            {
+                if (firstDate.DayOfWeek != DayOfWeek.Saturday && firstDate.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    // Rule1: if public holiday is on Saturday or Sunday dount count the day
+                    foreach (DateTime date1 in publicHolidaysWithRule1)
+                        if (firstDate == date1) { --days; }
+
+                    // Rule2: if public holiday is on Saturday or Sunday move it to Monday
+                    foreach (DateTime date2 in publicHolidaysWithRule2)
+                        if (date2.DayOfWeek == DayOfWeek.Saturday)
+                        {
+                            if (firstDate.AddDays(2) == date2.AddDays(2)) { --days; }
+                        }
+                        else if (date2.DayOfWeek == DayOfWeek.Sunday)
+                        {
+                            if (firstDate.AddDays(1) == date2.AddDays(1)) { --days; }
+                        }
+
+                    // Rule3: if public holiday never happens on Saturday or Sunnday
+                    foreach (DateTime date3 in publicHolidaysWithRule3)
+                        if (firstDate == date3) { --days; }
+
+                    ++days;
+                }
+                firstDate = firstDate.AddDays(1);
+            }
+            return days;
+        }
+    }
+}
---
+++ BusinessDayCounting/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessDayCounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// TASK 1: Weekday counting
+
+            // Monday 07-Oct-2013 and Wednesday 09-Oct-2013 : should return 1
+            int value1 = BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9));
+
+            // Saturday 05-Oct-2013 and Monday 14-Oct-2013 : should return 5
+            int value2 = BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 5), new DateTime(2013, 10, 14));
+
+            // Monday 07-Oct-2013 and Wednesday 01-Jan-2014 : should return 61
+            int value3 = BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1));
+
+            // Monday 07-Oct-2013 and Saturday 05-Oct-2013 : should return 0 (second date earlier than first date)
+            int value4 = BusinessDayCounter.WeekdaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 5));
+
+            
+            /// TASK 2: Business day counting
+
+            // Public holiday list containing Christmas Day, Boxing Day and New Year's Day
+            List<DateTime> publicHolidays = new List<DateTime> 
+            {
+                new DateTime(2013, 12, 25),
+                new DateTime(2013, 12, 26), 
+                new DateTime(2014, 1, 1) 
+            };
+
+            // Monday 07-Oct-2013 and Wednesday 09-Oct-2013 : should still return 1
+            int value5 = BusinessDayCounter.BusinessDaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9), publicHolidays);
+
+            // Tuesday 24-Dec-2013 and Friday 27-Dec-2013 : should return 0, both days are public holidays
+            int value6 = BusinessDayCounter.BusinessDaysBetweenTwoDates(new DateTime(2013, 12, 24), new DateTime(2013, 12, 27), publicHolidays);
+
+            // Monday 07-Oct-2013 and Wednesday 01-Jan-2014 : should return 59
+            int value7 = BusinessDayCounter.BusinessDaysBetweenTwoDates(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1), publicHolidays);
+            
+        }
+    }
+}
